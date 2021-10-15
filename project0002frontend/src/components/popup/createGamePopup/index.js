016c07960c22aa6de80001c9132a2fd14bb61af3
Hiroki-Imoto <2190493@ecccomp.com>
2021-10-15 12:15:03 +0900

    Create Game Popup(in Progress)

--- project0002frontend/src/components/popup/createGamePopup/index.js
+++ project0002frontend/src/components/popup/createGamePopup/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import './index.css'
 
-const index = () => {
+const index = props => {
     return (
-        <div>
-            
+        <div className='popup-box'>
+            <div className='box'>
+                <span className='close-Icon' onClick={props.handleClose}>X</span>
+                <>
+                <h2>Create Your Game</h2>
+                <p>Insert settings checkbox here...</p>
+                <button>Create Game(Dummy)</button>
+                </>
+            </div>
         </div>
     )
 }
