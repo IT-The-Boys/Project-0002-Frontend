bb9ba57b01f064042aa9d118b394106aaedbf061
Hiroki-Imoto <2190493@ecccomp.com>
2022-01-19 13:54:32 +0900

    Updated ConfirmHandPopup.js

--- project0002frontend/src/components/popup/comfirmHandPopup/ConfirmHandPopup.js
+++ project0002frontend/src/components/popup/comfirmHandPopup/ConfirmHandPopup.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useSelector } from 'react-redux'
 import './confirmHandPopup.css'
 
@@ -7,20 +7,33 @@ const ConfirmHandPopup = (props) => {
   console.log(playerAnswer)
   console.log(playerHand)
 
+  let list = []
+  playerHand.map((card, id) => {
+    playerAnswer.forEach(ansid => {
+      if(ansid === card.cardId){
+        console.log("hit: ", card.cardText)
+        list.push(card.cardText)
+      }
+    });
+  })
+  console.log(list);
+  
+  const confirmHand = () => {
+    //If player pushed confirm button
+
+  }
+
   return (
     <div className='popup-box'>
       <div className='box'>
         <span className='close-Icon' onClick={props.handleClose}>X</span>
         <div className='answerArea'>
-        {playerAnswer.map((id, ind) => {
-          playerHand.map((hand, index) => {
-            if(id === hand.cardId){
-              console.log("from prev page: ", id, "  this page", hand.cardText)
-              return <div key={index}>{hand.cardText}</div>
-            }
-          })
-        })}
+          Check your answer
+          {list.map((li, id) => {
+            return <div key={id}><fieldset><button>{li}</button></fieldset></div>
+          })}
         </div>
+        <span onClick={confirmHand}><button>Confirm</button></span>
       </div>
     </div>
   )
